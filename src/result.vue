72e3e2142988ffa20f69ea62500cb797754d7c1f
蒼時弦也 <elct9620@frost.tw>
2018-04-02 22:01:09 +0800

    Rewrite result page to dynamic display result

--- src/result.vue
+++ src/result.vue
@@ -1,5 +1,31 @@
 <template>
   <div id="result">
-    <img src="/images/result/6.jpg">
+    <img v-bind:src="image">
   </div>
 </template>
+
+<script>
+import results from './result.yml';
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    image() {
+      return `/images/result/${this.result.image}`;
+    },
+    result() {
+      const ID = parseInt(this.$route.params.resultID, 10);
+      let i;
+
+      for (i = 0; i < results.length; i += 1) {
+        if (results[i].match.includes(ID)) {
+          return results[i];
+        }
+      }
+      return {};
+    },
+  },
+};
+</script>
