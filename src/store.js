8f4d810246275e39101780e2a041cedc2b4e9470
蒼時弦也 <elct9620@frost.tw>
2018-12-12 23:40:53 +0800

    Adjust result compare functions

--- src/store.js
+++ src/store.js
@@ -15,19 +15,22 @@ bus.$on('UpdateScore', (score) => {
   PlayerScore.empathy += score.empathy;
 });
 
-const minBound = (value, compare) => (compare.min === -1 ? true : value >= compare.min);
-const maxBound = (value, compare) => (compare.max === -1 ? true : value <= compare.max);
-const scoreBound = (type, score, base) =>
-  minBound(score[type], base[type]) && maxBound(score[type], base[type]);
+const minBound = (compare, value) => (compare.min === -1 ? true : value >= compare.min);
+const maxBound = (compare, value) => (compare.max === -1 ? true : value <= compare.max);
+const scoreBound = (type, compare, player) =>
+  minBound(compare[type], player[type]) && maxBound(compare[type], player[type]);
 
-const characterFilter = score => character =>
-  scoreBound('trust', score, character.score) && scoreBound('action', score, character.score) && scoreBound('empathy', score, character.score);
+const characterFilter = playerScore => character =>
+  scoreBound('trust', character.score, playerScore) && scoreBound('action', character.score, playerScore) && scoreBound('empathy', character.score, playerScore);
 
 export default bus;
-export const getResult = () => results.filter(characterFilter(PlayerScore))[0];
 export const getScore = () => Object.assign({}, PlayerScore);
+export const getResult = () => results.filter(characterFilter(getScore()))[0];
 export const resetScore = () => {
   PlayerScore.trust = 0;
   PlayerScore.action = 0;
   PlayerScore.empathy = 0;
 };
+
+window.getResult = () => results.filter(characterFilter(PlayerScore));
+window.scoreBound = scoreBound;
